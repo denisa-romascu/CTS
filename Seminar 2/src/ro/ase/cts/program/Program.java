f0187583311878ffb10520504cd7efde91218e6d
denisa <romascudenisa18@stud.ase.ro>
2021-03-06 18:21:57 +0200

    Am adaugat 3 clase in pachetul "readers", am sters clasa Util, am modificat citirea angajatilor in cadrul clasei Program

--- Seminar 2/src/ro/ase/cts/program/Program.java
+++ Seminar 2/src/ro/ase/cts/program/Program.java
@@ -1,7 +1,7 @@
 package ro.ase.cts.program;
 
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.readers.AngajatiReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -11,7 +11,7 @@ public class Program {
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("Seminar 2/angajati.txt");
+			listaAngajati = AngajatiReader.readAngajati("Seminar 2/angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
