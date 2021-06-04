1cc8369a7c26c9e2900bf958b80dadfbbd88f437
denisa <romascudenisa18@stud.ase.ro>
2021-06-04 21:28:04 +0300

    Grupa,IStudent,Student & GrupaTest

---
+++ Seminar 13/src/teste/GrupaTest.java
@@ -0,0 +1,57 @@
+package teste;
+
+import clase.Grupa;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GrupaTest {
+
+    @Test
+    public void testConstructorRight() {
+        Grupa grupa = new Grupa(1086);
+        assertEquals(1086, grupa.getNrGrupa());
+    }
+
+    @Test
+    public void testConstructorLimitaInferioara() {
+        Grupa grupa = new Grupa(1000);
+        assertEquals(1000, grupa.getNrGrupa());
+    }
+
+    @Test
+    public void testConstructorLimitaSuperioara() {
+        Grupa grupa = new Grupa(1100);
+        assertEquals(1100, grupa.getNrGrupa());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorExceptie() {
+        Grupa grupa = new Grupa(900);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorExceptie2() {
+        Grupa grupa = new Grupa(900);
+    }
+
+    @Test(timeout = 500)
+    public void testConstructorPerformance() {
+        Grupa grupa = new Grupa(1086);
+    }
+
+    @Test
+    public void testConstructorRange() {
+        Grupa grupa = new Grupa(1001);
+        assertEquals(1001, grupa.getNrGrupa());
+        Grupa grupa2 = new Grupa(1099);
+        assertEquals(1099, grupa2.getNrGrupa());
+    }
+
+    @Test
+    public void testConstructorExistance() {
+        Grupa grupa = new Grupa(1041);
+        assertNotNull(grupa.getStudenti());
+    }
+
+}
